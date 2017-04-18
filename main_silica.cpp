2c32815b2497ac325833bde520103cc5c9f823ba
Naveen Kumar Kaliannan <naveenkumar5892@gmail.com>
2017-04-18 16:01:29 +0100

    New main implementation

--- main_silica.cpp
+++ main_silica.cpp
@@ -12,8 +12,9 @@
 #include<fstream>
 #include<stdlib.h>
 #include<vector>
-#include"ewald.h"
 #include<mpi.h>
+#include"ewald.h"
+#include "angular.h"
 
 using namespace std;
 
@@ -21,10 +22,14 @@ const double Pi = 3.141593;
 const double Unit_Coulomb = 1389.55;
 const double Unit_eV = 96.49930;
 const double R        = 0.0083144598; // Gas constant
-const double Temp     = 2750;         //Temperature of the system
-const double Pr       = 0.6023;      //Pressure of the system
+const double Temp     = 2000;         //Temperature of the system
+const double Pr       = 0.6023 * 1;      //Pressure of the system
+const int iter        = 1500000;
+const int equ_iter    = 500000;
+const int inter       = 2000;
+const double kappa    = 0.25;
 
-
+/*
 // generates a random  integer between [min,max)
 int Rand_INT(int min,int max) //[min,max)
 {
@@ -48,7 +53,7 @@ double min(double a,double b)
   {
     return a;
   }
-}
+} */
 
 // Maximum of 3 elements
 template <class T>
@@ -58,7 +63,7 @@ T max3(T a, T b, T c)
 }
 
 // Short-range interactions - BKS
-double BKS(vector<double> const &r, double L, double q1, double q2, int size, int rank)
+double ETNW(vector<double> const &r, double L, double q1, double q2, int size, int rank)
 {
   double u = 0, rij  = 0;  
   // MPI variables
@@ -78,15 +83,19 @@ double BKS(vector<double> const &r, double L, double q1, double q2, int size, in
                     + pow((r[i+1]-r[j+1]) - L * round((r[i+1]-r[j+1])/L),2) 
                     + pow((r[i+2]-r[j+2]) - L * round((r[i+2]-r[j+2])/L),2) );
           // Potential truncation
-          if(rij < 5)
+          if(rij < 6)
             {
+              if(r[i+3] == r[j+3] && r[i+3] == q1)//silicon - silicon
+                {
+                  u += 0.741948 * (exp(15.3744 * (1 - (rij/3.7598))) - 2 * exp((15.3744/2) * (1 - (rij/3.7598))));
+                }
               if(r[i+3] == r[j+3] && r[i+3] == q2)//oxygen - oxygen
                 {
-                  u += (134015.6224*exp(-rij*2.7600)) - (16887.3775/pow(rij,6));
+                  u += 2.23969 * (exp(10.4112 * (1 - (rij/3.790))) - 2 * exp((10.4112/2) * (1 - (rij/3.790))));
                 }
               if(r[i+3] != r[j+3]) //silicon - oxygen
                 { 
-                  u += (1737349.967*exp(-rij*4.87318)) - (12599.51795/pow(rij,6));
+                  u += 192.4514 * (exp(8.6342 * (1 - (rij/1.682))) - 2 * exp((8.6342/2) * (1 - (rij/1.620))));
                 }
             }
         }
@@ -115,7 +124,7 @@ double TCD(vector<double> const &r, double L, double q1, double q2, int size, in
                     + pow((r[i+1]-r[j+1]) - L * round((r[i+1]-r[j+1])/L),2) 
                     + pow((r[i+2]-r[j+2]) - L * round((r[i+2]-r[j+2])/L),2) );
           // Potential truncation
-          if(rij < 5)
+          if(rij < 6)
             {
               if(r[i+3] == r[j+3] && r[i+3] == q2)//oxygen - oxygen
                 {
@@ -153,9 +162,28 @@ int main(int argc, char** argv)
   vector<double> r_new;
 
   // Properties variables
-  //Radial Distribution Function(RDF)
-  unsigned int RDF_size = 600;
-  double RDF_h = 0.01;
+  // 1. Angular distribution variables
+  int a_size = 360;
+  double** osio = new double*[2];
+  double** siosi = new double*[2];
+  for(int i = 0;i < 2; i++)
+    {
+      osio[i] = new double[a_size];
+      siosi[i] = new double[a_size];
+    }    
+  int j = 0;
+  for(double i = 1;i < 180.5;i = i + 0.5)
+    {
+      osio[0][j] = i;
+      osio[1][j] = 0;
+      siosi[0][j] = i;
+      siosi[1][j] = 0;
+      j = j + 1;
+    }
+
+  // 2. Radial Distribution Function(RDF)
+  unsigned int RDF_size = 700;
+  double RDF_h = 0.1;
   double* rad = new double[RDF_size];
   double* RDF_all = new double[RDF_size];
   double* RDF_Si = new double[RDF_size];
@@ -171,6 +199,7 @@ int main(int argc, char** argv)
     }
   ofstream outfile1;
   ofstream outfile2;
+  ofstream outfile3;
 
   // Processor 0 - input/output processing
   if(rank == root) 
@@ -224,6 +253,7 @@ int main(int argc, char** argv)
       V_new = V;
       outfile1.open(argv[2]);
       outfile2.open(argv[3]);
+      outfile3.open(argv[4]);
     }
 
   MPI_Barrier(MPI_COMM_WORLD);
@@ -236,27 +266,32 @@ int main(int argc, char** argv)
   MPI_Bcast(&L, 1, MPI_DOUBLE, root, MPI_COMM_WORLD);
   MPI_Bcast(&r[0], r.size(), MPI_DOUBLE, root, MPI_COMM_WORLD);
 
-  double U_local = 0, U_global = 0, kappa = 0.2150, u = 0, u_new = 0;
+  double U_local = 0, U_global = 0, u = 0, u_new = 0;
   U_local = ( RealandReciprocalSpace(r, L, L, L, kappa, 1, size, rank) 
-             + kappa * PointEnergy(r, size, rank) / sqrt(Pi) ) * Unit_Coulomb + TCD(r, L, q1, q2, size, rank);
+             + kappa * PointEnergy(r, size, rank) / sqrt(Pi) ) * Unit_Coulomb + ETNW(r, L, q1, q2, size, rank);
   MPI_Reduce(&U_local, &u, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);
   u += (Unit_Coulomb *  2 * Pi * pow(Dipole(r),2) / (3 * V)) ;
 
   srand(time(NULL));
-  for(int k = 0;k < 5000000; k++)
+  for(int k = 0;k < iter; k++)
     {
       if(rank == root)
         {
-          //1. randomly select an atom
+          //1. Randomly select an atom
           unsigned int P = Rand_INT(0, r.size()/4) * 4;
 
-          //2. move the selected atom randomly
-          r_new[P+0] += 0.1*(2*Rand_DOUBLE()-1); 
-          r_new[P+1] += 0.1*(2*Rand_DOUBLE()-1);
-          r_new[P+2] += 0.1*(2*Rand_DOUBLE()-1);
+          //2. Move the selected atom randomly
+          r_new[P+0] += 0.15*(2*Rand_DOUBLE()-1); 
+          r_new[P+1] += 0.15*(2*Rand_DOUBLE()-1);
+          r_new[P+2] += 0.15*(2*Rand_DOUBLE()-1);
+         
+          P = Rand_INT(0, r.size()/4) * 4;
+          r_new[P+0] += 0.15*(2*Rand_DOUBLE()-1); 
+          r_new[P+1] += 0.15*(2*Rand_DOUBLE()-1);
+          r_new[P+2] += 0.15*(2*Rand_DOUBLE()-1);
           
           //3. Volume change
-          V_new += 10 * (2*Rand_DOUBLE()-1);
+          V_new += 15 * (2*Rand_DOUBLE()-1);
           L = pow(V_new,0.333333);
           double ratio = pow(V_new,0.3333333)/pow(V,0.3333333);
           for(unsigned int i = 0 ; i < r.size();i = i + 4)
@@ -275,7 +310,7 @@ int main(int argc, char** argv)
       //4. Calculate the potential energy of the configurations
       U_global = 0;
       U_local = ( RealandReciprocalSpace(r_new, L, L, L, kappa, 1, size, rank) 
-             + kappa * PointEnergy(r_new, size, rank) / sqrt(Pi) ) * Unit_Coulomb + TCD(r_new, L, q1, q2, size, rank);
+             + kappa * PointEnergy(r_new, size, rank) / sqrt(Pi) ) * Unit_Coulomb + ETNW(r_new, L, q1, q2, size, rank);
 
       MPI_Reduce(&U_local, &U_global, 1, MPI_DOUBLE, MPI_SUM, root, MPI_COMM_WORLD);
      if (rank == root)
@@ -302,16 +337,22 @@ int main(int argc, char** argv)
          if(k % 100 == 1)
            {
              outfile1 << k <<" "<< ((r.size()/4) * 33.252/V) << "  " <<  u/(96.49930*(r.size()/4)/3) << "\n";
+             cout << k <<" "<< ((r.size()/4) * 33.252/V) << "  " <<  u/(96.49930*(r.size()/4)/3) << "\n";
            }      
-         if(k > 3000000 && k%10000 == 1)
+         if(k > equ_iter && k%inter == 1)
            {
              mean += 1;
-             // 1. Radial Distribution Function
+             
+             // 1. Angular distribution function
+             angular_distribution_OSiO(L, osio, a_size, r, q1, q2);
+             angular_distribution_SiOSi(L, siosi, a_size, r, q1, q2);
+
+             // 2. Radial Distribution Function
              for(unsigned int i = 1;i < RDF_size-1;++i)
                {
                  unsigned int N_All = 0,N_Si = 0,N_O = 0,N_SiO = 0;
                  double rij = 0;
-                 for(unsigned int j = 0;j < r.size() ;j = j + 4)
+                 for(unsigned int j = 0;j < r.size();j = j + 4)
                    {
                      for(unsigned int k_ = 0;k_ < r.size() ;k_ = k_ + 4)
                        {
@@ -327,7 +368,7 @@ int main(int argc, char** argv)
                            {
                              N_O += 1;
                            }
-                         if(rij <= rad[i] + (RDF_h/2) && rij > rad[i] - (RDF_h/2) && r[k_+3] == r[j+3] && r[k_+3] == q1 && j != k_)
+                         if(rij <= rad[i] + (RDF_h/2) && rij > rad[i] - (RDF_h/2) && r[k_+3] == r[j+3] && r[k_+3] == q1 && j != k_ )
                            {
                              N_Si += 1;
                            }
@@ -354,6 +395,20 @@ int main(int argc, char** argv)
           outfile2 << rad[i] << " " << RDF_all[i] / (mean * r.size()/4) << " " << RDF_Si[i] / (mean * (r.size()/4)/3) 
                    << " " << RDF_O [i] / (mean * (r.size()/4) * 2/3) << " " << RDF_SiO[i] / (mean * (r.size()/4) * 2/3) << "\n";
         }
+
+      // Angular distribution
+      int total1 = 0, total2 = 0;
+      for(unsigned int i = 0;i < a_size; ++i)
+        {
+          total1 += siosi[1][i];
+          total2 += osio[1][i];
+        } 
+      for(unsigned int i = 0;i < a_size; ++i)
+        {
+          siosi[1][i] = siosi[1][i]/total1;
+          osio[1][i]  =  osio[1][i]/total2;
+          outfile3 << siosi[0][i] << "  " << siosi[1][i] << " " << osio[1][i] << "\n";
+        } 
     }
 
   MPI_Finalize();  
